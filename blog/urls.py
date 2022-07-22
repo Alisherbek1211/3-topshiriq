8106136ffd7a63f2f7b94a86dab3476dc5b808b8
Alisherbek1211 <alisheryuldashev1211@gmail.com>
2022-07-22 17:09:04 +0500

    .

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ router = routers.SimpleRouter()
 router.register(r'postdate/', PostDateView,basename="Post Date")
 router.register(r'popular/', PostPopularView,basename="Popular")
 router.register(r'recommended/', PostRecommendedView,basename="Recommended")
-router.register(r'post/<int:pk>/', PostDetailView,basename="post detail")
+router.register(r'post/', PostDetailView,basename="post detail")
 router.register(r'next/', PostReadNextView,basename="Next Posts")
 router.register(r'comment/', CommentView,basename="Commnent")
 
